03db992b454e455a40cbe08539603a7e550da410
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-07-24 14:23:31 -0500

    add flash error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,18 @@
 class PagesController < ApplicationController
   def create_csv
-    csv_lines = GenerateCsv.call(params[:title_team_player], params[:input])
-    if csv_lines
-      returned_csv = CSV.generate(headers: true) do |csv|
-        csv_lines.each do |line|
-          csv << line
+    if params[:title_team_player] && params[:input]
+      csv_lines = GenerateCsv.call(params[:title_team_player], params[:input])
+      if csv_lines
+        returned_csv = CSV.generate(headers: true) do |csv|
+          csv_lines.each do |line|
+            csv << line
+          end
         end
+        send_data returned_csv, filename: "shopify_upload.csv"
       end
-      send_data returned_csv, filename: "shopify_upload.csv"
+    else
+      flash[:error] = "Input File and TitleTeamPlayer File Required"
+      redirect_to clothing_index_path
     end
   end
 end
