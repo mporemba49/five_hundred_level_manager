81921289386accbd4b075dacb107aa880a5d7c7c
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-07-24 13:45:46 -0500

    not there

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.autoload_paths << Rails.root.join('lib')
 
   # Code is not reloaded between requests.
   config.cache_classes = true
