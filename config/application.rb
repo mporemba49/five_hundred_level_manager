e236e088efa1dd91e1e5802356bd0073d4756294
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-07-24 13:36:38 -0500

    move file to app/lib

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ Bundler.require(*Rails.groups)
 module FiveHundredLevelManager
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('lib/service_objects')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
