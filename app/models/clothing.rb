e4cb3c253700c5c77a11b074ddf8d5e5f0bbcdce
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-08-03 21:18:43 -0500

    more puts

--- app/models/clothing.rb
+++ app/models/clothing.rb
@@ -151,8 +151,10 @@ class Clothing < ApplicationRecord
     image_url = @entry.url_string_for_clothing(self, clothing_color.image)
     return false unless image_url
 
+    puts sizes
     sizes.each do |size|
       lines << csv_line_for_size_and_color(size, clothing_color, image_url, first_line)
+      puts lines
       first_line = false if first_line
     end
 
