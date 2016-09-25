89d731198135bd15696ac60176bd6fcc22a30ee5
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-09-25 13:40:26 -0500

    no validation on ordinal

--- app/models/size.rb
+++ app/models/size.rb
@@ -2,7 +2,6 @@ class Size < ApplicationRecord
   has_many :clothing_sizes, dependent: :destroy
   has_and_belongs_to_many :clothing, join_table: :clothing_sizes
   validates_uniqueness_of :sku
-  validates_uniqueness_of :is_kids, scope: [:ordinal], unless: Proc.new { |s| s.ordinal.blank? }
 
   scope :kids, -> { where(is_kids: true) }
   scope :adults, -> { where(is_kids: false) }
