36108939029edc1906ef47e0060aab570f854dba
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-09-30 16:12:48 -0500

    add failure email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,26 @@
 class UserMailer < ApplicationMailer
-  def test
-      mail(to: "nicholas.lee.3@gmail.com", subject: "Test")
+  def alert_error(email, error)
+    @error = error
+    mail(to: [email, "nicholas.lee.3@gmail.com"], subject: "500 Level | Upload Error")
   end
 
   def csv_upload(email, title_team_player, sales_channel_id)
-    title_team_player_path = Downloader.call(title_team_player)
+    begin
+      title_team_player_path = Downloader.call(title_team_player)
 
-    csv_lines, @missing_files = GenerateCsv.call(title_team_player_path, sales_channel_id)
-    if csv_lines
-      returned_csv = CSV.generate(headers: true) do |csv|
-        csv_lines.each do |line|
-          csv << line
+      csv_lines, @missing_files = GenerateCsv.call(title_team_player_path, sales_channel_id)
+      if csv_lines
+        returned_csv = CSV.generate(headers: true) do |csv|
+          csv_lines.each do |line|
+            csv << line
+          end
         end
+        attachments["shopify_upload.csv"] =  returned_csv
       end
-      attachments["shopify_upload.csv"] =  returned_csv
-    end
 
-    mail(to: email, subject: "500 Level | CSV Download")
+      mail(to: email, subject: "500 Level | CSV Download")
+    end
+  rescue => error
+    self.alert_error(email, error)
   end
 end
