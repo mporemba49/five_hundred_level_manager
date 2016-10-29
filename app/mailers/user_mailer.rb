0cad2d15efd87166886a44c7d3731141bf001102
Nicholas Lee <nicholas.lee.3@gmail.com>
2016-10-29 00:01:03 -0500

    only save the objects referenced in the TTP file

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,6 @@ class UserMailer < ApplicationMailer
 
   def csv_upload(email, title_team_player, sales_channel_id)
     title_team_player_path = Downloader.call(title_team_player)
-
     csv_lines, @missing_files = GenerateCsv.call(title_team_player_path, sales_channel_id)
     if csv_lines
       returned_csv = CSV.generate(headers: true) do |csv|
