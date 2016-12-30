303468d7e84b80bc8452fef1a01fb4192a9aa2bf
Braden Perez-Mugg <braden.mugg@gmail.com>
2016-12-30 13:34:43 -0700

    fix typo

--- app/controllers/clothing_sizes_controller.rb
+++ app/controllers/clothing_sizes_controller.rb
@@ -7,7 +7,7 @@ class ClothingSizesController < ApplicationController
     @clothing = Clothing.includes(:clothing_sizes).find(params[:clothing_id])
     updated_sizes = size_params.keep_if { |key, value| value != "" }
     ClothingSize.where(clothing: @clothing).each do |clothing_size|
-      weight = update_sizes["weight #{clothing_size.id}"] || ""
+      weight = updated_sizes["weight #{clothing_size.id}"] || ""
       clothing_size.update_attribute(:weight, weight) unless weight == ""
     end
 
