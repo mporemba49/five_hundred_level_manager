bf21d0998b78ed9c04f528aa3690be5e2e5a1002
Braden Perez-Mugg <braden.mugg@gmail.com>
2017-04-25 13:16:11 -0600

    Add string attributes to bulk upload

--- app/lib/inventory_upload.rb
+++ app/lib/inventory_upload.rb
@@ -7,7 +7,7 @@ HEADERS = ["SKU", "Player", "Design", "Team", "Apparel", "Style Size", "Color", 
 
 class InventoryUpload
   def self.call(file_path)
-    columns = [:full_sku, :team_player_design_id, :team_player_id, :size_id, :color_id, :producible_id, :producible_type, :location]
+    columns = [:full_sku, :team_player_design_id, :team_player_id, :size_id, :color_id, :producible_id, :producible_type, :location, :design, :product, :team, :player, :league, :artist]
     bulk_upload_path = Downloader.call(file_path)
     lines = CSV.read(bulk_upload_path, encoding: 'iso-8859-1')
     lines.shift
@@ -19,15 +19,24 @@ class InventoryUpload
         value = []
         value << line[0]
         team = Team.where(id: line[0].slice(15..18).to_i).first
-        player_id = team.team_players.find_by_player(line[1]).id
-        value << TeamPlayerDesign.where(sku: line[0].slice(24..25).to_i, team_player_id: player_id).first.id
-        value << player_id
-        value << Color.where(sku: line[0].slice(8..10)).first.id
-        value << Size.where(sku: line[0].slice(3..4)).first.id
+        player = team.team_players.find_by_player(line[1])
+        design = TeamPlayerDesign.where(sku: line[0].slice(24..25).to_i, team_player_id: player_id).first
+        color = Color.where(sku: line[0].slice(8..10)).first
+        size = Size.where(sku: line[0].slice(3..4)).first
+        value << design.id
+        value << player.id
+        value << color.id
+        value << size.id
         item = Accessory.unscoped.where(sku: line[0].slice(5..7)).first || Clothing.unscoped.where(sku: line[0].slice(5..7)).first
         value << item.id
         value << item.class.name
         value << line[7] || "N/A"
+        value << design.name
+        value << item.name
+        value << team.name
+        value << player.player
+        value << team.league
+        value << design.artist
         value.include?(nil) ? incomplete_values << line : values << value
       else
         unless line[1...6].include?(nil)
