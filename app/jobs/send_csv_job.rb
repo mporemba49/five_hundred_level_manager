477a87c7b946678dbac69becf88671a1618d2add
Braden Perez-Mugg <braden.mugg@gmail.com>
2017-05-30 11:37:26 -0600

    Fix user mailer call variables

--- app/jobs/send_csv_job.rb
+++ app/jobs/send_csv_job.rb
@@ -5,7 +5,7 @@ class SendCsvJob < ApplicationJob
     title_team_player_path = Downloader.call(title_team_player)
     csv_lines, @missing_files = GenerateCsv.call(title_team_player_path, sales_channel_ids.first)
     sales_channel_id = sales_channel_ids.shift
-    UserMailer.csv_upload(email, csv_lines, sales_channel_id).deliver_now
+    UserMailer.csv_upload(email, csv_lines, @missing_files, sales_channel_id).deliver_now
     if csv_lines
       sales_channel_ids.each do |channel_id|
         channel = SalesChannel.find_by_id(channel_id)
@@ -14,7 +14,7 @@ class SendCsvJob < ApplicationJob
           line[13] = line[13] + channel.sku
           csv << line
         end
-        UserMailer.csv_upload(email, csv_lines, @missing_files sales_channel_id).deliver_now
+        UserMailer.csv_upload(email, csv_lines, @missing_files, sales_channel_id).deliver_now
       end
     end
     Validator.reset
