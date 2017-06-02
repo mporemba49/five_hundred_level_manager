3a9e7043175349c6ac745fc062dd01a1c5b2014f
Braden Perez-Mugg <braden.mugg@gmail.com>
2017-06-01 20:15:42 -0600

    Compare string for etsy modification

--- app/jobs/send_csv_job.rb
+++ app/jobs/send_csv_job.rb
@@ -5,7 +5,7 @@ class SendCsvJob < ApplicationJob
     title_team_player_path = Downloader.call(title_team_player)
     csv_lines, @missing_files = GenerateCsv.call(title_team_player_path, sales_channel_ids.first)
     sales_channel_id = sales_channel_ids.shift
-    if sales_channel_id == 2
+    if sales_channel_id == "2" 
       etsy_lines = EtsyModification.call(csv_lines)
       UserMailer.csv_upload(email, etsy_lines, @missing_files, sales_channel_id).deliver_now
     else
@@ -18,7 +18,7 @@ class SendCsvJob < ApplicationJob
           line[13].chop!.chop!
           line[13] = line[13] + channel.sku
         end
-        if channel_id == 2
+        if channel_id == "2"
           etsy_lines = EtsyModification.call(csv_lines)
           logger.info "Etsy"
           UserMailer.csv_upload(email, etsy_lines, @missing_files, channel_id).deliver_now
