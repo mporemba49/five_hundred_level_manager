90f7918ff4b20c52547c35311ecdd4223907d2ff
Braden Perez-Mugg <braden.mugg@gmail.com>
2017-06-01 19:30:07 -0600

    Etsy modification feature

--- app/jobs/send_csv_job.rb
+++ app/jobs/send_csv_job.rb
@@ -5,7 +5,12 @@ class SendCsvJob < ApplicationJob
     title_team_player_path = Downloader.call(title_team_player)
     csv_lines, @missing_files = GenerateCsv.call(title_team_player_path, sales_channel_ids.first)
     sales_channel_id = sales_channel_ids.shift
-    UserMailer.csv_upload(email, csv_lines, @missing_files, sales_channel_id).deliver_now
+    if sales_channel_id == 2
+      etsy_lines = EtsyModification.call(csv_lines)
+      UserMailer.csv_upload(email, etsy_lines, @missing_files, sales_channel_id).deliver_now
+    else
+      UserMailer.csv_upload(email, csv_lines, @missing_files, sales_channel_id).deliver_now
+    end
     if csv_lines
       sales_channel_ids.each do |channel_id|
         channel = SalesChannel.find_by_id(channel_id)
@@ -13,7 +18,12 @@ class SendCsvJob < ApplicationJob
           line[13].chop!.chop!
           line[13] = line[13] + channel.sku
         end
-        UserMailer.csv_upload(email, csv_lines, @missing_files, channel_id).deliver_now
+        if channel_id == 2
+          etsy_lines = EtsyModification.call(csv_lines)
+          UserMailer.csv_upload(email, etsy_lines, @missing_files, sales_channel_id).deliver_now
+        else
+          UserMailer.csv_upload(email, csv_lines, @missing_files, channel_id).deliver_now
+        end
       end
     end
     Validator.reset
---
+++ app/lib/etsy_modification.rb
@@ -0,0 +1,52 @@
+class EtsyModification
+
+  def self.call(csv_lines)
+    images = csv_lines.map { |line| line[24] }
+    images.drop(1)
+    images.uniq!
+    csv_lines.each { |line| line.insert(25, nil) }
+    csv_lines.each_with_index do |line, index|
+      if index = [0]
+        line[25] = "Image Position"
+      else
+        style = line[8]
+        tags = line[5].split(",")
+        tags.map! { |t| t.split("=")}
+        tags.reject! { |t| t[0] == "v" }
+        sport = tags.select { |t| t[0] == "sport" }
+        sport = sport[1]
+        player = tags.select { |t| t[0] == "player" }
+        player = player[1]
+        team = tags.select { |t| t[0] == "team" }
+        team = team[1]
+        city = tags.select { |t| t[0] == "city" }
+        city = city[1]
+        tags.map! { |t| t[1] }
+        tags = tags.join(", ")
+        if sport == "Baseball" || sport == "Baseball Hall of Fame"
+          tags += ", MLB, World Series"
+        elsif sport == "Football"
+          tags += ", NFL, Super Bowl"
+        elsif sport == "Baseball"
+          tags += ", NHL, Stanley Cup"
+        end
+        line[1] = line[1] + “Officially Licensed” + sport == "Personalities" ? "" : city + style
+        line[5] = tags
+        line[7] = line[9]
+        line[8] = line[10]
+        line[9] = line[11]
+        line[10] = line[12]
+        line[11] = nil
+        line[12] = nil
+        line[16] = 99
+        if images.present
+          line[25] = index + 1
+        end
+        line[24] = images.shift if images.present?
+      end
+      line[26] = player + style + team + " 500 Level"
+    end
+    return csv_lines
+  end
+
+end
