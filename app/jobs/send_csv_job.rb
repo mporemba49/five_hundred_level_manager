c5bf3be902de86302ac016a196e820e815417c29
Braden Perez-Mugg <braden.mugg@gmail.com>
2017-06-29 14:07:30 -0600

    Fix for differing sales channel ids

--- app/jobs/send_csv_job.rb
+++ app/jobs/send_csv_job.rb
@@ -1,34 +1,35 @@
 class SendCsvJob < ApplicationJob
   queue_as :default
 
-  def perform(email, title_team_player, sales_channel_ids)
+  def perform(email, title_team_player, sales_channel_skus)
     title_team_player_path = Downloader.call(title_team_player)
-    if etsy_index = sales_channel_ids.index("2")
-      last_index = sales_channel_ids.size - 1
-      sales_channel_ids.insert(last_index, sales_channel_ids.delete_at(etsy_index))
+    if sales_channel_skus.include?("ET")
+      etsy_index = sales_channel_skus.index("ET")
+      last_index = sales_channel_skus.size - 1
+      sales_channel_skus.insert(last_index, sales_channel_skus.delete_at(etsy_index))
     end
     csv_lines, @missing_files = GenerateCsv.call(title_team_player_path, sales_channel_ids.first)
-    sales_channel_id = sales_channel_ids.shift
-    if sales_channel_id == "2" 
+    sales_channel_sku = sales_channel_skus.shift
+    if sales_channel_sku == "ET"
       etsy_lines = EtsyModification.call(csv_lines)
-      UserMailer.csv_upload(email, etsy_lines, @missing_files, sales_channel_id).deliver_now
+      UserMailer.csv_upload(email, etsy_lines, @missing_files, SalesChannel.find_by_sku(sales_channel_sku).first.id).deliver_now
     else
-      UserMailer.csv_upload(email, csv_lines, @missing_files, sales_channel_id).deliver_now
+      UserMailer.csv_upload(email, csv_lines, @missing_files, SalesChannel.find_by_sku(sales_channel_sku).first.id).deliver_now
     end
     if csv_lines
-      sales_channel_ids.each do |channel_id|
-        channel = SalesChannel.find_by_id(channel_id)
+      sales_channel_skus.each do |channel_sku|
+        channel = SalesChannel.find_by_sku(channel_sku)
         csv_lines.drop(1).each do |line|
           line[13].chop!.chop!
           line[13] = line[13] + channel.sku
         end
-        if channel_id == "2"
+        if channel_sku == "ET"
           etsy_lines = EtsyModification.call(csv_lines)
           logger.info "Etsy"
-          UserMailer.csv_upload(email, etsy_lines, @missing_files, channel_id).deliver_now
+          UserMailer.csv_upload(email, etsy_lines, @missing_files, channel.id).deliver_now
         else
           logger.info "Not Etsy"
-          UserMailer.csv_upload(email, csv_lines, @missing_files, channel_id).deliver_now
+          UserMailer.csv_upload(email, csv_lines, @missing_files, channel.id).deliver_now
         end
       end
     end
