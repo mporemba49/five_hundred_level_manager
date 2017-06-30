e22892ae968eb9e37d6058817795da577f4ec721
Braden Perez-Mugg <braden.mugg@gmail.com>
2017-06-30 09:56:09 -0600

    Accessory color update all feature

--- app/controllers/accessory_colors_controller.rb
+++ app/controllers/accessory_colors_controller.rb
@@ -8,13 +8,22 @@ class AccessoryColorsController < ApplicationController
   end
 
   def update
-
-    @accessory = Accessory.includes(:accessory_colors).find(params[:accessory_id])
-    updated_colors = color_params.keep_if { |key, value| value != "" }
-    AccessoryColor.where(accessory: @accessory).where.not(color_id: updated_colors.keys).destroy_all
-    updated_colors.each do |color_id, image|
-      accessory_color = AccessoryColor.where(accessory: @accessory, color_id: color_id).first_or_initialize
-      accessory_color.update_attribute(:image, image) unless image == accessory_color.image
+    if params[:mass_update].present?
+      @accessory = Accessory.find(params[:accessory_id])
+      accessory_name = params[:mass_update]
+      @colors = Color.all
+     @colors.each do |color|
+        accessory_color = AccessoryColor.where(accessory: @accessory, color_id: color.id).first_or_initialize
+        accessory_color.update_attributes(image: accessory_color.color.name.delete("/ ") + accessory_name)
+      end
+    else
+      @accessory = Accessory.includes(:accessory_colors).find(params[:accessory_id])
+      updated_colors = color_params.keep_if { |key, value| value != "" }
+      AccessoryColor.where(accessory: @accessory).where.not(color_id: updated_colors.keys).destroy_all
+      updated_colors.each do |color_id, image|
+        accessory_color = AccessoryColor.where(accessory: @accessory, color_id: color_id).first_or_initialize
+        accessory_color.update_attribute(:image, image) unless image == accessory_color.image
+      end
     end
     redirect_to accessory_path(@accessory)
   end
--- app/models/accessory_color.rb
+++ app/models/accessory_color.rb
@@ -1,6 +1,7 @@
 class AccessoryColor < ApplicationRecord
   belongs_to :accessory
   belongs_to :color
+  attr_accessor :mass_update
 
   def color_name
     color.name
