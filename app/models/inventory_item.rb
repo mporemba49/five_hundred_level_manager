be05b398bc52a63219a5a1ff73833aa1bc4f27a6
Braden Perez-Mugg <braden.mugg@gmail.com>
2017-08-24 09:24:42 -0600

    Unscope producible association for inventory items

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -3,11 +3,16 @@ class InventoryItem < ApplicationRecord
   belongs_to :team_player
   belongs_to :size
   belongs_to :color
-  belongs_to :producible, polymorphic: true
+  belongs_to :producible, -> { unscope(:where) }, polymorphic: true
 
   attr_accessor :bulk_csv
   validates_presence_of :full_sku
   validates_uniqueness_of :full_sku
   acts_as_paranoid
+
+  def build_entry
+    row = { 'Title' => team_player_design.name, 'Team' => team_player.team.name, 'Player' => team_player.player, 'League' => team_player.team.league, 'City' => team_player.team.city, 'Artist' => team_player_design.artist }
+    InputEntry.new(row)
+  end
 
 end
