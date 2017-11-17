66133a66b8064dbdda7974c215c016bc7e1ebbd0
Braden Perez-Mugg <braden.mugg@gmail.com>
2017-11-17 12:05:35 -0700

    Add collection back to clearance CSVs

--- app/lib/clearance_modification.rb
+++ app/lib/clearance_modification.rb
@@ -6,12 +6,12 @@ class ClearanceModification
     csv_lines.drop(1).each do |line|
       Rails.logger.info(line) 
       line[0] += "-clearance"
+      line[44] = line[1] + " Clearance"
       line[1] += " - Clearance"
       line[5] += ",Clearance"
       line[15] = "shopify"
       line[20] = line[19]
       line[19] = line[19] / 2
-      line[44] += " Clearance"
     end
     return csv_lines
   end
